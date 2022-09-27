c28e82bfbdaf3d6e308f92c6d0312b882b4844ed
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-09-27 18:30:03 +0200

    fine esercizio

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    @include('includes.header')
-</body>
-
-</html>
+@extends('layout.main')
+@section('main-content')
+    <div class="container box">
+        @foreach ($media as $element)
+            <div class="card">
+                <img src="{{ $element['thumb'] }}" alt="{{ $element['title'] }}" />
+                <h3>{{ $element['title'] }}</h3>
+            </div>
+        @endforeach
+    </div>
+@endsection
